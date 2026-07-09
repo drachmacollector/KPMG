db328b64cae089013eb568b5cc2aab9706197b54
Nakul <agentvasco69@gmail.com>
2026-07-09 15:11:46 +0530

    gemini models updated

--- web_resolver.py
+++ web_resolver.py
@@ -100,10 +100,11 @@ Strict output:
 
 
 GEMINI_MODELS = [
-    "gemini-2.5-pro",
-    "gemini-2.0-flash",
-    "gemini-2.5-flash",
     "gemini-2.5-flash-lite",
+    "gemini-2.5-flash",
+    "gemini-3-flash",
+    "gemini-3.1-flash-lite",
+    "gemini-3.5-flash"
 ]
 
 
